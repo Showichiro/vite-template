9398b550ef8bdc7802e2e1d9498af7f8e04a3bca
uchihori <show2305@gmail.com>
2022-06-27 02:20:00 +0900

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  plugins: [
+    'react',
+    "react-hooks",
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    "react/jsx-filename-extension": [
+      "error",
+      {
+          "extensions": [
+              ".js",
+              ".jsx",
+              ".ts",
+              ".tsx"
+          ]
+      }
+  ],
+  },
+};
